b5f1d9971e47250d09d0767dec1dc374d6e93e92
kalq <bassam.saeed@gmail.com>
2020-02-05 00:33:17 -0500

    Added interrupt function

--- main.go
+++ main.go
@@ -12,42 +12,48 @@ func usage() {
 	fmt.Println("[4] Change timer")
 }
 
-func startTimer(ticker *time.Ticker, timer int, stop chan bool) {
+func startTimer(ticker time.Ticker, timer int, stop chan bool) {
 
 	i := timer
 	for range ticker.C {
-		select {
-		case <-stop:
+		if i != 0 {
+			select {
+			case <-stop:
+				return
+			default:
+				i--
+				fmt.Printf("\r\033[A\033[K%d seconds left\n", i)
+			}
+		} else {
+			usage()
 			return
-		default:
-			i--
-			fmt.Printf("\r\033[K%d seconds left", i)
 		}
 	}
 }
 
 func main() {
+
 	usage()
 
 	var input int
-	_, err := fmt.Scanf("%d", &input)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
+	var ticker time.Ticker
+	stop := make(chan bool)
+	
 	for {
-		ticker := time.NewTicker(time.Second)
-		stop := make(chan bool)
+		_, err := fmt.Scanf("%d", &input)
+
+		if err != nil {
+			fmt.Println(err)
+		}
 		
 		switch input {
 		case 1:
-			go startTimer(ticker, 25, stop)
-			time.Sleep(time.Second * 25)
 			ticker.Stop()
-			usage()
+			ticker = *time.NewTicker(time.Second)
+			go startTimer(ticker, 4, stop)
 		case 2:
-			
+			stop <- true
+			usage()
 		}
 	}
 }
